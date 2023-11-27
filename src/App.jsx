c1d27c185aba26c89316bec2767486bfbbeb4e9b
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-27 23:04:26 +0430

    Fix navbar problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-
-import Home from "./pages/Home"
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <Home />
     <MainPage />
   )
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import Headline from '../headline/Headline'
+import Header from "../componenets/Header";
+import Navbar from "../componenets/Navbar";
 
 const MainPage = () => {
   return (
     <div className='h-full w-full max-w-[1440px] mx-auto'>
-        <Headline />
+      <Header />
+      <Navbar />
+      <Headline />
     </div>
   )
 }
