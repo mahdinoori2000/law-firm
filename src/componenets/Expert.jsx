8cc0f09c696a1034e5c1a0f04812af7c6e519f45
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-28 00:14:12 +0430

    Add expert team

---
+++ src/componenets/Expert.jsx
@@ -0,0 +1,43 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import lawyer from '../assets/lawyer.png';
+
+function ExpertTeam() {
+  const teamMembers = [
+    { name: 'Mohsen', position: 'Senior Lawyer', image: lawyer },
+    { name: 'Jane', position: 'Legal Consultant', image: lawyer },
+    { name: 'John', position: 'Attorney', image: lawyer },
+    // Add more team members as needed
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <section className="my-20">
+      <h2 className="text-center mb-4 text-lg font-bold">Our Expert Team</h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 cursor-pointer">
+            <div className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+              <img src={member.image} alt={member.name} className="w-full h-48 object-cover object-center" />
+              <div className="px-4 py-2">
+                <h2 className="text-xl font-semibold">{member.name}</h2>
+                <p className="text-gray-600">{member.position}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+    </section>
+  );
+}
+
+export default ExpertTeam;
