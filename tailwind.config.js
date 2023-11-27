ffbde199d7cca0f00ecc2e1f74c59e99be324724
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-11-27 18:44:09 +0430

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  colors: {
+    "custom-primary-blue": "#1e40af",
+    "custom-secondary-blue": "#06b6d4",
+    "custom-primary-yellow": "#d97706",
+    "custom-secondary": "#7c2d12"
+  },
   theme: {
     extend: {},
   },
