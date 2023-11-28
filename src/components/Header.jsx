9f2e5c34654e72efc4e1c6104627426c026fa27b
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-28 23:49:37 +0430

    Fix contact issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import  { useState, useEffect } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/taawon.png';
 import emailIcon from '../assets/icons/email-icon.svg';
 import phoneIcon from '../assets/icons/phone-icon.svg';
 
@@ -18,16 +18,16 @@ function Header() {
   }, []); 
   return (
     <section className="px-5 h-[10vh] flex justify-around items-center">
-      <div className="w-24">
+      <div className="w-20">
         <img src={logo} alt="Logo" />
       </div>
       {screenWidth ? (
         <div className="flex items-center justify-center gap-7">
-          <p className="flex items-center gap-2 text-sm" ><img src={emailIcon} alt="Email Icon" style={{fill: 'red'}} />+93 730 23 23 89</p>
-          <p className="flex items-center gap-2 text-sm" ><img src={phoneIcon} alt="Phone Icon" style={{fill: 'red'}} />info@taawon.af</p>
+          <p className="flex items-center gap-2 text-sm" ><img src={emailIcon} alt="Email Icon" style={{fill: 'red'}} />info@taawon.af</p>
+          <p className="flex items-center gap-2 text-sm" ><img src={phoneIcon} alt="Phone Icon" style={{fill: 'red'}} />+93 730 23 23 89</p>
         </div>
       ) : null}
-      <select className="h-7 focus:outline-none text-sm">
+      <select className="h-7 focus:outline-none text-sm cursor-pointer">
         <option value="english" className="text-[14px]">En</option>
         <option value="Persian" className="text-[14px]">Fa</option>
         <option value="Pashto" className="text-[14px]">Pa</option>
