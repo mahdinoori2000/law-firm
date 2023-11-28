13ed3d148780b7c749636ad695666ef72daab78e
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-28 23:56:34 +0430

    Remove header in the responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,22 +17,34 @@ function Header() {
     };
   }, []); 
   return (
-    <section className="px-5 h-[10vh] flex justify-around items-center">
-      <div className="w-20">
-        <img src={logo} alt="Logo" />
-      </div>
-      {screenWidth ? (
-        <div className="flex items-center justify-center gap-7">
-          <p className="flex items-center gap-2 text-sm" ><img src={emailIcon} alt="Email Icon" style={{fill: 'red'}} />info@taawon.af</p>
-          <p className="flex items-center gap-2 text-sm" ><img src={phoneIcon} alt="Phone Icon" style={{fill: 'red'}} />+93 730 23 23 89</p>
+    screenWidth && (
+      <section className="px-5 h-[10vh] flex justify-around items-center">
+        <div className="w-20">
+          <img src={logo} alt="Logo" />
         </div>
-      ) : null}
-      <select className="h-7 focus:outline-none text-sm cursor-pointer">
-        <option value="english" className="text-[14px]">En</option>
-        <option value="Persian" className="text-[14px]">Fa</option>
-        <option value="Pashto" className="text-[14px]">Pa</option>
-      </select>
-    </section>
+        <div className="flex items-center justify-center gap-7">
+          <p className="flex items-center gap-2 text-sm">
+            <img src={emailIcon} alt="Email Icon" style={{ fill: 'red' }} />
+            info@taawon.af
+          </p>
+          <p className="flex items-center gap-2 text-sm">
+            <img src={phoneIcon} alt="Phone Icon" style={{ fill: 'red' }} />
+            +93 730 23 23 89
+          </p>
+        </div>
+        <select className="h-7 focus:outline-none text-sm cursor-pointer">
+          <option value="english" className="text-[14px]">
+            En
+          </option>
+          <option value="Persian" className="text-[14px]">
+            Fa
+          </option>
+          <option value="Pashto" className="text-[14px]">
+            Pa
+          </option>
+        </select>
+      </section>
+    )
   );
 }
 
